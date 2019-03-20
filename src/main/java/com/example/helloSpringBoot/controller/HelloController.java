b7ea6188d5d8767630739d2403f0051857668c28
lifq <lifuqiang@xinfushe.com>
2019-03-20 13:02:50 +0800

    跨域

--- src/main/java/com/example/helloSpringBoot/controller/HelloController.java
+++ src/main/java/com/example/helloSpringBoot/controller/HelloController.java
@@ -1,13 +1,15 @@
 package com.example.helloSpringBoot.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
+    @CrossOrigin
     @RequestMapping("/hello")
     public String HelloSpring (){
-//        System.out.println("hello spring boot");
-        return "hello spring boot！";
+        return "hello Java碎碎念！";
     }
 }
