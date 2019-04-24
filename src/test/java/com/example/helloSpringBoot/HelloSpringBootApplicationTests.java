38873043b97705b59caa3a6ad21d8481eacd0983
java_suisui <lifuqinag412@163.com>
2019-04-24 12:34:11 +0800

    test

--- src/test/java/com/example/helloSpringBoot/HelloSpringBootApplicationTests.java
+++ src/test/java/com/example/helloSpringBoot/HelloSpringBootApplicationTests.java
@@ -1,17 +1,47 @@
 package com.example.helloSpringBoot;
 
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc  //测试接口用
 public class HelloSpringBootApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Before
+    public void testBefore(){
+        System.out.println("测试前");
+    }
+
+    @After
+    public void testAfter(){
+        System.out.println("测试后");
+    }
+
+    //下面是测试接口
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void apiTest()throws Exception{
+        MvcResult mvcResult=mockMvc.perform(MockMvcRequestBuilders.get("/hello")).
+                andExpect(MockMvcResultMatchers.status().isOk()).andReturn();
+        int status=mvcResult.getResponse().getStatus();
+        //打印出状态码，200就是成功
+        System.out.println(status);
+        Assert.assertEquals(200,status);
+    }
 
 }
 
